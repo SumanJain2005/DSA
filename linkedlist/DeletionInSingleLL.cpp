b0096e7c0d2f68b3f5ed252af6d088d701a7508f
SumanJain2005 <sumannareshclg@gmail.com>
2024-11-28 21:54:37 +0530

    Tested all linkedlist funcitons on Problem Solving websites

--- linkedlist/DeletionInSingleLL.cpp
+++ linkedlist/DeletionInSingleLL.cpp
@@ -31,28 +31,42 @@ Node* TailInsert(Node*& head , int n){
 	temp->next = newNode;
 	return head;
 }
-Node* PositionInsert(Node*& head , int k ,int n){
-	if(head == nullptr){
-		if(k==1) return new Node(n);
-		else return NULL;	
-	}
-	else if(k==1){
-		Node* temp = new Node(n,head);
-		return head;;
-	}
-	int counter = 0;
-	Node* temp = head;
-	while(temp!= nullptr){
-		counter++;
-		if(k == counter+1){
-			Node* x = new Node(n);
-			x->next = temp->next; // aage x er porer number is same as temp er porer number
-			temp->next = x; // then temp er porer number is x
-			break;
-		}
-		temp = temp->next;	 
-	}
-	return head;
+Node* PositionInsert(Node*& head, int k, int n) {
+    // Case 1: If the list is empty
+    if (head == nullptr) {
+        if (k == 1) { 
+            return new Node(n); // If inserting at position 1, create a new node and return it as the new head
+        } 
+        else {
+            return nullptr; // If k > 1 and the list is empty, return NULL
+        }
+    }
+
+    // Case 2: Inserting at the head position
+    else if (k == 1) {
+        Node* temp = new Node(n, head); // Create a new node pointing to the current head
+        head = temp; // Update head to point to the new node
+        return head; 
+    }
+
+    // Case 3: Inserting at other positions
+    int counter = 1; // Start counter from 1 since we're already at head
+    Node* temp = head;
+
+    while (temp != nullptr) {
+        // When we reach the node just before the kth position
+        if (k == counter + 1) { 
+            Node* x = new Node(n); // Create a new node with value `n`
+            x->next = temp->next; // x's next is temp's next
+            temp->next = x; // temp's next is now x
+            break;
+        }
+        temp = temp->next; // Move to the next node
+        counter++;	 
+    }
+
+    // If k is greater than the length of the list, no insertion is done.
+    return head;
 }
 Node* InsertBeforeVlaue(Node*& head, int b ,int n){
 	Node* prev = nullptr;
@@ -106,29 +120,37 @@ Node* tailDel(Node* head){
 	return head;
 }
 Node* PositionDel(Node* head , int k){
-	// total 4 cases must be handled!
-	if(head == nullptr)// ekhane nullptr
-		return head;
-	else if(k==1){ // ekhane 1 ta case cover hocche, which is head
-		Node* temp = head;
-		head = head->next;
-		free(temp);
-		return head;
-	}
-	int count = 1;
-	Node* temp = head;
-	Node* prev = nullptr;
-	while(temp != nullptr){
-		count++;
-		if(count == k){ // ekhane 2 to case cover hocche, Kth positon r tail 
-			prev->next = prev->next->next; // ager value ta theke direct porer value te point, majkhaner kth value got skipped
-			free(temp);
-			break;
-		}
-		prev = temp; // aager value ta mone rakhlm
-		temp = temp->next; // porer value te chole gelam
-	}
-	return head;
+	if (head == nullptr) {
+        return nullptr;
+    }
+
+    // Case 2: Deleting the head node
+    if (k == 1) {
+        Node* temp = head;  // Store the current head in a temporary pointer
+        head = head->next;  // Move the head to the next node
+        free(temp);         // Free the old head
+        return head;        // Return the updated head
+    }
+
+    // Case 3 & 4: Deleting nodes other than the head
+    Node* temp = head;      // Pointer to traverse the list
+    Node* prev = nullptr;   // To keep track of the previous node
+    int count = 1;
+
+    // Traverse to find the kth node
+    while (temp != nullptr && count < k) {
+        prev = temp;
+        temp = temp->next;
+        count++;
+    }
+
+    // If the kth node exists
+    if (temp != nullptr) {
+        prev->next = temp->next;  // Skip the kth node
+        free(temp);               // Free the kth node
+    }
+
+    return head;
 }
 Node* ValueDel(Node* head , int value){ 
 	Node* temp = head;
